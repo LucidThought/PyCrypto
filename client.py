63c9264fc5120cebe451a1066308546838116ae6
LucidThought <alata.it@gmail.com>
2017-03-06 13:40:49 -0700

    Can't get it to print the sha hash or a sample randomly generated IV, will test more later

--- client.py
+++ client.py
@@ -1,9 +1,12 @@
-#!/usr/bin/python
+
+
+#!/usr/bin/python3
 
 import base64
 import hashlib
 import sys
 import time
+import socket
 
 from Crypto import Random
 from Crypto.Cipher import AES
@@ -14,6 +17,7 @@ DEST = ''
 CIPHER = ''
 KEY = ''
 PW = ''
+FILE = sys.stdin.buffer.read()
 
 
 # The following command read stdin as a bytestream:
@@ -23,33 +27,56 @@ PW = ''
 # outFile.write(inBytes)
 # outFile.close()
 
-def start(self)
-  global COMMAND = ''
-  global FILENAME = ''
-  global DEST = ''
-  global CIPHER = ''
-  global KEY = ''
+def start():
+  global COMMAND
+  global FILENAME
+  global DEST
+  global CIPHER
+  global KEY
+  global PW
+  global FILE
 
-  if(sys.argv[4]=='none'):
+  print(hashlib.sha256(str_to_bytes("test")).digest())
+  print(Random.new().read(AES.block_size).toString())
+
+  if (sys.argv[4]=='none'):
     COMMAND = str(sys.argv[1])
     FILENAME = str(sys.argv[2])
     DEST = str(argv[3])
     CIPHER = 'none'
   else:
     if(len(sys.argv)==5):
-    COMMAND = str(sys.argv[1])
-    FILENAME = str(sys.argv[2])
-    DEST = str(argv[3])
-    CIPHER = str(argv[4])
-    PW = str(argv[5])
-    print("Password: " + PW)
-    KEY = hashlib.sha256(str_to_bytes(PW)).digest()
-  elif(len(sys.argv)==4)
-    COMMAND = str(sys.argv[1])
-    FILENAME = str(sys.argv[2])
-    DEST = str(argv[3])
-    CIPHER = 'none'
+      COMMAND = str(sys.argv[1])
+      FILENAME = str(sys.argv[2])
+      DEST = str(argv[3])
+      CIPHER = str(argv[4])
+      PW = str(argv[5])
+      print("Password: " + PW)
+      KEY = hashlib.sha256(str_to_bytes(PW)).digest()
+    elif(len(sys.argv)==4):
+      COMMAND = str(sys.argv[1])
+      FILENAME = str(sys.argv[2])
+      DEST = str(argv[3])
+      CIPHER = 'none'
+  
+def startClient(destAddr):
+  global COMMAND
+  global DEST
+  global FILE
+  separator = DEST.find(":")
+  ipDEST = DEST[0:separator]
+  sockDEST = DEST[separator+1:]
+  clientSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  clientSocket.bind(ipDEST,sockDEST)
+#  if(COMMAND=='read'):
+    
+#  elif(COMMAND=='write'):
+    
+#  else:
+#    print("I don't know how to " + COMMAND)
 
 
-if __name__ == '__main__':
-    start()
+if(__name__ == "__main__"):
+  print(hashlib.sha256(str_to_bytes("test")).digest())
+  print(Random.new().read(AES.block_size).toString())
+  start()
