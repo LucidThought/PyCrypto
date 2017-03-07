117d23eb7840dff03c374dda0674931ecab28eea
LucidThought <alata.it@gmail.com>
2017-03-06 18:18:23 -0700

    Added code

--- client.py
+++ client.py
@@ -7,6 +7,7 @@ import hashlib
 import sys
 import time
 import socket
+import os
 
 from Crypto import Random
 from Crypto.Cipher import AES
@@ -27,7 +28,7 @@ FILE = sys.stdin.buffer.read()
 # outFile.write(inBytes)
 # outFile.close()
 
-def start():
+def main():
   global COMMAND
   global FILENAME
   global DEST
@@ -35,15 +36,19 @@ def start():
   global KEY
   global PW
   global FILE
-
-  print(hashlib.sha256(str_to_bytes("test")).digest())
-  print(Random.new().read(AES.block_size).toString())
+  print("Main")
+#  print(hashlib.sha256(str_to_bytes("test")).hexdigest())  # This gives a 32-byte key value
+#  print(hashlib.md5("test".encode()).hexdigest()) # This gives a 16-byte key value
+  rand = os.urandom(32)
+  print(hashlib.md5(rand).hexdigest()) # Randomly generate 16-byte key
+#  print(os.urandom(32)) # Randomly generate 32-byte key 
+  print(Random.new().read(AES.block_size))
 
   if (sys.argv[4]=='none'):
     COMMAND = str(sys.argv[1])
     FILENAME = str(sys.argv[2])
     DEST = str(argv[3])
-    CIPHER = 'none'
+    CIPHER = 'none' 	
   else:
     if(len(sys.argv)==5):
       COMMAND = str(sys.argv[1])
@@ -59,6 +64,12 @@ def start():
       DEST = str(argv[3])
       CIPHER = 'none'
   
+def str_to_bytes(data):
+  utype = type(b''.decode('utf-8'))
+  if isinstance(data, utype):
+    return data.encode('utf-8')
+  return data
+
 def startClient(destAddr):
   global COMMAND
   global DEST
@@ -71,12 +82,13 @@ def startClient(destAddr):
 #  if(COMMAND=='read'):
     
 #  elif(COMMAND=='write'):
-    
+        
 #  else:
 #    print("I don't know how to " + COMMAND)
 
 
 if(__name__ == "__main__"):
-  print(hashlib.sha256(str_to_bytes("test")).digest())
-  print(Random.new().read(AES.block_size).toString())
-  start()
+  print("Start")
+#  print(str(hashlib.sha256(str_to_bytes("test")).digest()))
+#  print(str(Random.new().read(AES.block_size)))
+  main()
